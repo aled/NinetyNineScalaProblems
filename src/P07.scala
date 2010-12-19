bc3c8da4a9b1950bfbf3bb9858a7ae13ab2830ec
Aled <aled@sourceforge.net>
2010-12-19 14:44:51 +0000

    Add P07

--- src/P07.scala
+++ src/P07.scala
@@ -9,9 +9,19 @@ package com.wibblr.ninety_nine_scala_problems
 object P07 {
   val versions = List[List[Any] => List[Any]] (flatten1)
 
+  import scala.collection.mutable.ListBuffer
+
+  private def appendToListBuffer(lb: ListBuffer[Any], l: List[Any]): Unit = {
+    for (item <- l)
+      item match {
+      case sub: List[Any] => appendToListBuffer(lb, sub)
+      case _ => lb.append(item)
+    }
+  }
+
   def flatten1(l: List[Any]): List[Any] = {
-
-
-
+    val lb = new ListBuffer[Any]()
+    appendToListBuffer(lb, l)
+    lb.toList
   }
 }
